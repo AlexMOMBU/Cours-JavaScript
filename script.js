3e35c6b08e6e48de53c02a1c4a04310ffb12da8a
alexMOMBU <alexMOMBU@gmail.com>
2023-12-20 22:37:11 +0100

    Booleens et operateur de comparaisons

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
 /*var x = 12;*/
 //var y = 5;
-var z = 10;
+//var z = 10;
 
    // console.log(x);
    // console.log(y);
-    console.log(z);
+  //  console.log(z);
+
+  // 48- les booléens
+
+  var x = 5;
+  var y =12;
+  var mybooléens = (x>3 || y<10);
+
+  console.log(mybooléens);
+
