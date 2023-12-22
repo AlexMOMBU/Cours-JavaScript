165fac21d888196f646615398576eb28b1579019
alexMOMBU <alexMOMBU@gmail.com>
2023-12-22 22:36:57 +0100

    Les conditions javascript

--- script.js
+++ script.js
@@ -9,9 +9,36 @@
 
   // 48- les booléens
 
-  var x = 5;
+  /*var x = 5;
   var y =12;
   var mybooléens = (x>3 || y<10);
 
-  console.log(mybooléens);
+  console.log(mybooléens);*/
 
+
+  // 49- les conditions
+var speed = 90
+if (speed <80) 
+{
+    console.log('tu roules à la bonne vitesse');
+}
+
+else if(speed < 100)
+{
+   console.log("tu devrais ralentir un peu");
+}
+else
+{
+   console.log("tu roule beaucoup trop vite , ralenti un peu car c'est dangereux!")
+}
+
+var favoriteColor = blue
+{
+   case "blue": 
+      console.log("Waouh le bleu c'est magnifique");
+      break;
+   case "red":
+      console.log("jaimerai bien une voiture rouge"); 
+   default: 
+      console.log("je ne connais pas ta couleur")    
+}
